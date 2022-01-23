329ba883be6b73521c128846c74cfb88dbc8fbcc
ARushan2021 <ARushan@appline.ru>
2022-01-23 20:33:52 +0300

    Задача №2

---
+++ Zadacha2.java
@@ -0,0 +1,8 @@
+public class Zadacha2 {
+    public static void main(String args[]) {
+        double a = 200.1844;
+        byte b = 127;
+        System.out.println("Переменная с плавующей точкой с наибольшим диапозоном чисел - double, значение = " + a);
+        System.out.println("Переменная целочисленная с наименьшим диапозоном чисел - byte, значение = " + b);
+    }
+}
