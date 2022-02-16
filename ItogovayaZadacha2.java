939f9015d2ad2e29e86490171784d74a6c751f8f
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-02-16 22:02:36 +0300

    Итоговая задача №2

--- ItogovayaZadacha2.java
+++ ItogovayaZadacha2.java
@@ -4,35 +4,39 @@ public class ItogovayaZadacha2 {
     public static void main(String args[]) {
         System.out.println("Ввод: ");
         Scanner in = new Scanner(System.in);
+        String st = in.nextLine();
 
-        String[] array = new String[5];
-        for (int i = 0; i < array.length; i++) {array[i] = in.next();}
+        String str = st.replaceAll(" ", "");
 
-        boolean ar1; ar1 = (array[0].equals("x") || array[0].equals("0") || array[0].equals("1") || array[0].equals("2") || array[0].equals("3") || array[0].equals("4") || array[0].equals("5") || array[0].equals("6") || array[0].equals("7") || array[0].equals("8") || array[0].equals("9"));
-        boolean ar2; ar2 = (array[1].equals("+") || array[1].equals("-"));
-        boolean ar3; ar3 = (array[2].equals("x") || array[2].equals("0") || array[2].equals("1") || array[2].equals("2") || array[2].equals("3") || array[2].equals("4") || array[2].equals("5") || array[2].equals("6") || array[2].equals("7") || array[2].equals("8") || array[2].equals("9"));
-        boolean ar4; ar4 = array[3].equals("=");
-        boolean ar5; ar5 = (array[4].equals("x") || array[4].equals("0") || array[4].equals("1") || array[4].equals("2") || array[4].equals("3") || array[4].equals("4") || array[4].equals("5") || array[4].equals("6") || array[4].equals("7") || array[4].equals("8") || array[4].equals("9"));
-        boolean arX; arX = (array[0].equals("x") ^ array[2].equals("x") ^ array[4].equals("x"));
+        boolean val = (str.length() == 5) ;
+        int a = Character.getNumericValue(str.charAt(0));
+        boolean val1 = str.startsWith("x") || a >= 0 & a <= 9;
+        boolean val2 = (str.charAt(1) == '+') || (str.charAt(1) == '-');
+        int b = Character.getNumericValue(str.charAt(2));
+        boolean val3 = str.charAt(2) == 'x' || b >= 0 & b <= 9;
+        boolean val4 = str.charAt(3) == '=';
+        int c = Character.getNumericValue(str.charAt(4));
+        boolean val5 = str.endsWith("x") || c >= 0 & c <= 9;
+        boolean valX = str.startsWith("x") ^ str.charAt(2) == 'x' ^ str.endsWith("x");
 
         int result = 0;
 
-        if (ar1 & ar2 & ar3 & ar4 & ar5 & arX) {
-            if (array[1].equals("+")) {
-               if (array[0].equals("x")) {result = Integer.parseInt(array[4]) - Integer.parseInt(array[2]);}
-               else if (array[2].equals("x")) {result = Integer.parseInt(array[4]) - Integer.parseInt(array[0]);}
-               else if (array[4].equals("x")) {result = Integer.parseInt(array[0]) + Integer.parseInt(array[2]);}
+        if (val & val1 & val2 & val3 & val4 & val5 & valX) {
+            if (str.charAt(1) == '+') {
+                if (str.startsWith("x")) {result = c - b;}
+                else if (str.charAt(2) == 'x') {result = c - a;}
+                else if (str.endsWith("x")) {result = a + b;}
             }
-            else if (array[1].equals("-")){
-                if (array[0].equals("x")) {result = Integer.parseInt(array[2]) + Integer.parseInt(array[4]);}
-                else if (array[2].equals("x")){result = Integer.parseInt(array[0]) - Integer.parseInt(array[4]);}
-                else if (array[4].equals("x")){result = Integer.parseInt(array[0]) - Integer.parseInt(array[2]);}
+            else if (str.charAt(1) == '-'){
+                if (str.startsWith("x")) {result = b + c;}
+                else if (str.charAt(2) == 'x'){result = a - c;}
+                else if (str.endsWith("x")){result = a - b;}
             }
             System.out.println("Вывод : " + result);
         }
         else {System.out.println("не верный формат");}
-    }
 
+    }
 }
 
 
